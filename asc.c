fe0a042fdc97f8c212eb0a5c5bda4641a09ca5ba
Joel <jzarley@umail.ucsb.edu>
2016-11-02 16:19:00 -0700

    ASCII values

---
+++ asc.c
@@ -0,0 +1,15 @@
+/* Joel Zarley */
+
+#include <stdio.h>
+
+int main (void)
+{
+
+  printf("  char      integer \n");
+
+  for (int i=33; i<=125; i++)
+    {
+      printf("   %c          %i  \n",i,i);
+    }
+  return 0;
+}
